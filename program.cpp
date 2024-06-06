61c4fe6ea3d1e42cc3f4f8bf5efb9bf19755cf81
domkarv <omkardate29@gmail.com>
2024-06-06 23:07:20 +0530

    new setup

--- program.cpp
+++ program.cpp
@@ -1,60 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+/**
+ * This function help to setup input and output
+ * in `input.txt` and `output.txt` file respectively.
+ */
 void setupIO() {
 #ifndef INPUT_OUTPUT
   freopen("input.txt", "r", stdin);
   freopen("output.txt", "w", stdout);
 #endif
 }
+/* ------- */
+
+void printPatternOne(int n) {}
 
 int main() {
   setupIO();
   /* ------- */
 
-  int ROW_LEN, COL_LEN;
-  cin >> ROW_LEN >> COL_LEN;
+  int itr;
+  cin >> itr;
 
-  for (int i = 0; i < ROW_LEN - 1; i++) {
+  for (int i = 0; i < itr; i++) {
+    int n;
+    cin >> n;
 
-    for (int j = 0; j < i + 1; j++) {
-      cout << COL_LEN - j << " ";
-    }
-
-    for (int j = 1; j < COL_LEN - i; j++) {
-      cout << COL_LEN - i << " ";
-    }
-
-    for (int j = 1; j < COL_LEN - i; j++) {
-      cout << COL_LEN - i << " ";
-    }
-
-    for (int j = i; j > 0; j--) {
-      cout << COL_LEN - j + 1 << " ";
-    }
-
-    cout << endl;
-  }
-
-  for (int i = 0; i < ROW_LEN; i++) {
-
-    for (int j = 0; j < COL_LEN - i; j++) {
-      cout << COL_LEN - j << " ";
-    }
-
-    for (int j = i; j > 0; j--) {
-      cout << i + 1 << " ";
-    }
-
-    for (int j = i; j > 0; j--) {
-      cout << i + 1 << " ";
-    }
-
-    for (int j = 1; j < COL_LEN - i; j++) {
-      cout << j + i + 1 << " ";
-    }
-
-    cout << endl;
+    printPatternOne(n);
   }
 
   return 0;
