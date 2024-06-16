8bba6febc1400a9721bc6dc068204381d43ed9bf
domkarv <omkardate29@gmail.com>
2024-06-16 12:59:35 +0530

    count digits

--- program.cpp
+++ program.cpp
@@ -13,9 +13,31 @@ void setupIO() {
 }
 /* ------- */
 
+int countDigits(int num) {
+  int temp = 0;
+  int val = num;
+
+  while (val != 0) {
+    int lastDigit = val % 10;
+
+    if (num % lastDigit == 0)
+      temp++;
+
+    val = val / 10;
+  }
+
+  return temp;
+}
+
 int main() {
   setupIO();
   /* ------- */
+
+  int N;
+  cin >> N;
+
+  double res = (int)(log10(N) + 1);
+  cout << res;
 
   return 0;
 }
