4b36071e8cd27382cc97aa7800a668dcf237d058
Omkar Date <omkardate29@gmail.com>
2024-12-17 02:07:23 +0530

    butterfly pattern

--- index.ts
+++ index.ts
@@ -3,17 +3,17 @@ const n = 4;
 for (let i = 0; i < n; i++) {
   let row = "";
 
-  for (let j = 0; j < n - i - 1; j++) {
+  for (let j = 0; j < i + 1; j++) {
+    row += "* ";
+  }
+
+  for (let j = 0; j < 2 * (n - (i + 1)) - 1; j++) {
     row += "  ";
   }
 
-  row += "* ";
-
-  for (let j = 0; j < 2 * i - 1; j++) {
-    row += "  ";
+  for (let j = 0; j < i + 1; j++) {
+    if (j !== n - 1) row += "* ";
   }
-
-  if (i != 0) row += "* ";
 
   console.log(row);
 }
@@ -21,17 +21,17 @@ for (let i = 0; i < n; i++) {
 for (let i = 0; i < n - 1; i++) {
   let row = "";
 
-  for (let j = 0; j < i + 1; j++) {
+  for (let j = 0; j < n - i - 1; j++) {
+    row += "* ";
+  }
+
+  for (let j = 0; j < 2 * (i + 1) - 1; j++) {
     row += "  ";
   }
 
-  row += "* ";
-
-  for (let j = 0; j < n - 1 - 2 * i; j++) {
-    row += "  ";
+  for (let j = 0; j < n - i - 1; j++) {
+    row += "* ";
   }
-
-  if (i != n - 2) row += "* ";
 
   console.log(row);
 }
