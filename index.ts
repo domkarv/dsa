2d7e9fb0318413e22458d1bad862c94500871924
Omkar Date <omkardate29@gmail.com>
2024-12-16 20:16:40 +0530

    Kadane's Algorithm - Maximum Subarray Sum

--- index.ts
+++ index.ts
@@ -1,13 +1,17 @@
-let arr = [3, -4, 5, 4, -1, 7, -8];
+/**
+ * Kadane's Algorithm - Maximum Subarray Sum
+ */
+
+let arr = [-2, 1, -3, 4, -1, 2, 1, -5, 4];
 
 let maxSum = Number.MIN_SAFE_INTEGER;
+let currSum = 0;
 
 for (let i = 0; i < arr.length; i++) {
-  let currSum = 0;
-  for (let j = i; j < arr.length; j++) {
-    currSum += arr[j] as number;
-    maxSum = Math.max(maxSum, currSum);
-  }
+  currSum += arr[i] as number;
+  maxSum = Math.max(maxSum, currSum);
+
+  currSum = currSum < 0 ? 0 : currSum;
 }
 
 console.log(maxSum);
