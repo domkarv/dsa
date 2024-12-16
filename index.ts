a20d113da66cbf883c03eaa8c8161a1f5b08248d
Omkar Date <omkardate29@gmail.com>
2024-12-17 00:03:55 +0530

    inverted triangle pattern - 2

--- index.ts
+++ index.ts
@@ -1,9 +1,16 @@
-const n = 5;
+const n = 4;
 
 for (let i = 0; i < n; i++) {
   let row = "";
-  for (let j = 0; j < n - i; j++) {
-    row += String.fromCodePoint(65 + i) + " ";
+
+  for (let j = 0; j < i; j++) {
+    row += "  ";
   }
+
+  for (let j = 0; j < n - i; j++) {
+    // row += i + 1 + " "; // for numbers
+    row += String.fromCodePoint(65 + i) + " "; // for alphabet
+  }
+
   console.log(row);
 }
