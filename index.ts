23335ebddd5b94660af89d8f5c6d3a62531e1d1a
Omkar Date <omkardate29@gmail.com>
2024-12-16 23:45:37 +0530

    floyd's triangle pattern

--- index.ts
+++ index.ts
@@ -1,9 +1,11 @@
-const n = 6;
+const n = 5;
+
+let char = 65;
 
 for (let i = 0; i < n; i++) {
   let row = "";
-  for (let j = i + 1; j > 0; j--) {
-    row += j + " ";
+  for (let j = 0; j <= i; j++) {
+    row += String.fromCodePoint(char++) + " ";
   }
   console.log(row);
 }
