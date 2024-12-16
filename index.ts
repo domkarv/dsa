b9549fbe7bae6633b12e59768e4e10a4553206e1
Omkar Date <omkardate29@gmail.com>
2024-12-16 23:42:04 +0530

    tringle pattern

--- index.ts
+++ index.ts
@@ -1,11 +1,9 @@
-const n = 4;
-
-let char = 65;
+const n = 6;
 
 for (let i = 0; i < n; i++) {
   let row = "";
-  for (let j = 0; j < n; j++) {
-    row += String.fromCodePoint(char++) + " ";
+  for (let j = i + 1; j > 0; j--) {
+    row += j + " ";
   }
   console.log(row);
 }
