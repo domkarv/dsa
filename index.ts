fef1b9a0b6a9b1829a3c5bf8e5a5b8196b7751e1
Omkar Date <omkardate29@gmail.com>
2024-12-17 01:57:01 +0530

    hollow diamond pattern

--- index.ts
+++ index.ts
@@ -3,13 +3,35 @@ const n = 4;
 for (let i = 0; i < n; i++) {
   let row = "";
 
-  for (let j = 1; j < n - i; j++) {
+  for (let j = 0; j < n - i - 1; j++) {
     row += "  ";
   }
 
-  for (let j = 0; j < 2 * i + 1; j++) {
-    row += "* ";
+  row += "* ";
+
+  for (let j = 0; j < 2 * i - 1; j++) {
+    row += "  ";
   }
+
+  if (i != 0) row += "* ";
+
+  console.log(row);
+}
+
+for (let i = 0; i < n - 1; i++) {
+  let row = "";
+
+  for (let j = 0; j < i + 1; j++) {
+    row += "  ";
+  }
+
+  row += "* ";
+
+  for (let j = 0; j < n - 1 - 2 * i; j++) {
+    row += "  ";
+  }
+
+  if (i != n - 2) row += "* ";
 
   console.log(row);
 }
