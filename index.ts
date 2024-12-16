2f47d720af4cdfa36e8a110cce868d6472525f88
Omkar Date <omkardate29@gmail.com>
2024-12-17 00:15:07 +0530

    number pattern

--- index.ts
+++ index.ts
@@ -3,13 +3,16 @@ const n = 4;
 for (let i = 0; i < n; i++) {
   let row = "";
 
-  for (let j = 0; j < i; j++) {
+  for (let j = 1; j < n - i; j++) {
     row += "  ";
   }
 
-  for (let j = 0; j < n - i; j++) {
-    // row += j + 1 + " "; // for numbers
-    row += String.fromCodePoint(65 + j) + " "; // for alphabet
+  for (let j = 0; j <= i; j++) {
+    row += j + 1 + " ";
+  }
+
+  for (let j = i; j > 0; j--) {
+    row += j + " ";
   }
 
   console.log(row);
