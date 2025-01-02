187a04baf7540d56998eaa8d1b53974dedf1cda8
Omkar Date <omkardate29@gmail.com>
2025-01-02 13:02:06 +0530

    Find the XOR of Numbers Which Appear Twice (LeetCode #3158)

--- index.ts
+++ index.ts
@@ -1,50 +1,32 @@
 /**
- * Find the missing number in an array containing numbers from 0 to n
- * (LeetCode #268)
+ * Find the XOR of Numbers Which Appear Twice
+ * (LeetCode #3158)
  */
 
 /* 1 */
-// function missingNumber(nums: number[]): number {
-//   nums.sort((a, b) => a - b);
-//   for (let i = 0; i < nums.length; i++) {
-//     if (nums[i] !== i) {
-//       return i;
-//     }
-//   }
-//   return nums.length;
+// function duplicateNumbersXOR(nums: number[]): number {
+//   let unique = Array.from(new Set(nums));
+//   let xor = 0;
+//   unique.forEach((el) => {
+//     xor ^= el;
+//   });
+//   nums.forEach((el) => {
+//     xor ^= el;
+//   });
+//   return xor;
 // }
 
 /* 2 */
-// function missingNumber(nums: number[]): number {
-//   let currSum = nums.reduce((prev, curr) => prev + curr, 0);
-//   let expectedSum = (nums.length * (nums.length + 1)) / 2;
-//   return expectedSum - currSum;
-// }
-
-/* 3 */
-function missingNumber(nums: number[]): number {
-  /**
-   * Calculate XOR of All Indices:
-   *   - We will compute the XOR of all indices from 0 to n.
-   *   - This includes every number that should be present in the array.
-   *
-   * Calculate XOR of Array Elements:
-   *   - We also compute the XOR of all elements present in the array.
-   *
-   * Combine Both Results:
-   *   - Finally, we XOR the two results together.
-   *     The pairs that exist in both the index range and the array will cancel each other out due to the self-cancellation property of XOR,
-   *     leaving only the missing number.
-   */
-
+function duplicateNumbersXOR(nums: number[]): number {
   let xor = 0;
-  for (let i = 0; i < nums.length; i++) {
-    xor ^= i ^ nums[i];
-  }
-  xor ^= nums.length;
+  let hash: Record<number, number> = {};
+  nums.forEach((num) => {
+    hash[num] = (hash[num] || 0) + 1;
+    if (hash[num] == 2) xor ^= num;
+  });
   return xor;
 }
 
-const nums = [9, 6, 4, 2, 3, 5, 7, 0, 1];
+const nums = [1, 2, 3, 2, 3, 4, 4];
 
-console.log(missingNumber(nums));
+console.log(duplicateNumbersXOR(nums));
