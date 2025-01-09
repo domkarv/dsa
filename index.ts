6ded08c6b795bcf0c94a779d8b90af0c31fb60a9
Omkar Date <omkardate29@gmail.com>
2025-01-09 21:30:36 +0530

    variable swapping

--- index.ts
+++ index.ts
@@ -1,37 +1,28 @@
 /**
- * Single Number III
- * (LeetCode #260)
+ * (LeetCode #)
  */
 
+let x = 5;
+let y = 10;
+console.log(x, y);
+
 /* 1 */
-// function singleNumber(nums: number[]): number[] {
-//   let map = new Map<number, number>();
-//   nums.forEach((el) => {
-//     map.set(el, (map.get(el) || 0) + 1);
-//   });
-//   let res: number[] = [];
-//   map.forEach((val, key) => {
-//     if (val === 1) res.push(key);
-//   });
-//   return res;
-// }
+x = x ^ y; // 5 ^ 10
+y = x ^ y; // 5 ^ 10 ^ 10
+x = x ^ y; // 5 ^ 10 ^ 5
+console.log(x, y); // 10 5
 
 /* 2 */
-function singleNumber(nums: number[]): number[] {
-  let xor = 0;
-  for (const num of nums) {
-    xor ^= num;
-  }
-  let divider = xor & -xor;
-  let num1 = 0;
-  let num2 = 0;
-  for (const num of nums) {
-    if ((num & divider) === 0) num1 ^= num;
-    else num2 ^= num;
-  }
-  return [num1, num2];
-}
+let temp = x;
+x = y;
+y = temp;
+console.log(x, y); // 5 10
 
-const nums = [1, 2, 1, 4, 2, 5];
+/* 3 */
+x = x + y; // 5 + 10 = 15
+y = x - y; // 15 - 10 = 5
+x = x - y; // 15 - 5 = 10
+console.log(x, y); // 10 5
 
-console.log(singleNumber(nums));
+[x, y] = [y, x];
+console.log(x, y); // 5 10
