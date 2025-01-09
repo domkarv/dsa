38818c5f70f8dfb9afc9f017cbc2ce5c836f716a
Omkar Date <omkardate29@gmail.com>
2025-01-09 16:52:34 +0530

    Single Number III (LeetCode #260)

--- index.ts
+++ index.ts
@@ -1,64 +1,37 @@
 /**
- * Single Number II
- * (LeetCode #137)
+ * Single Number III
+ * (LeetCode #260)
  */
 
 /* 1 */
-// function singleNumber(nums: number[]): number {
-//   let hash: Record<number, number> = {};
-//   let single = NaN;
-//   nums.forEach((num) => {
-//     hash[num] = (hash[num] || 0) + 1;
+// function singleNumber(nums: number[]): number[] {
+//   let map = new Map<number, number>();
+//   nums.forEach((el) => {
+//     map.set(el, (map.get(el) || 0) + 1);
 //   });
-//   Object.entries(hash).forEach(([key, value]) => {
-//     if (value === 1) single = Number(key);
+//   let res: number[] = [];
+//   map.forEach((val, key) => {
+//     if (val === 1) res.push(key);
 //   });
-//   return single;
+//   return res;
 // }
 
 /* 2 */
-// function singleNumber(nums: number[]): number {
-//   const bitCount = new Array(32).fill(0);
-//   for (const num of nums) {
-//     for (let i = 0; i < 32; i++) {
-//       if ((num & (1 << i)) !== 0) {
-//         bitCount[i]++;
-//       }
-//     }
-//   }
-//   let result = 0;
-//   for (let i = 0; i < 32; i++) {
-//     if (bitCount[i] % 3 !== 0) {
-//       result |= 1 << i;
-//     }
-//   }
-//   return result;
-// }
-
-/* 3 */ // don't know how the fuck is this working, there's no concept just calculation in this
-// function singleNumber(nums: number[]): number {
-//   let ones = 0;
-//   let twos = 0;
-//   for (let num of nums) {
-//     ones = (num ^ ones) & ~twos;
-//     twos = (num ^ twos) & ~ones;
-//   }
-//   return ones;
-// }
-
-/* 4 */
-function singleNumber(nums: number[]): number {
-  nums.sort((a, b) => a - b);
-
-  // every element appears three times except for one
-  // that's why `i=i+3`
-  for (let i = 1; i < nums.length; i = i + 3) {
-    if (nums[i] !== nums[i - 1]) return nums[i - 1];
+function singleNumber(nums: number[]): number[] {
+  let xor = 0;
+  for (const num of nums) {
+    xor ^= num;
   }
-
-  return nums[nums.length - 1];
+  let divider = xor & -xor;
+  let num1 = 0;
+  let num2 = 0;
+  for (const num of nums) {
+    if ((num & divider) === 0) num1 ^= num;
+    else num2 ^= num;
+  }
+  return [num1, num2];
 }
 
-const nums = [0, 1, 1, 0, 1, 0, 2];
+const nums = [1, 2, 1, 4, 2, 5];
 
 console.log(singleNumber(nums));
