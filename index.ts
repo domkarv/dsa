c2aff3556cb9180bdec498607901526024fd0500
Omkar Date <omkardate29@gmail.com>
2025-01-09 14:01:36 +0530

    Single Number II (LeetCode #137)

--- index.ts
+++ index.ts
@@ -1,32 +1,64 @@
 /**
- * Find the XOR of Numbers Which Appear Twice
- * (LeetCode #3158)
+ * Single Number II
+ * (LeetCode #137)
  */
 
 /* 1 */
-// function duplicateNumbersXOR(nums: number[]): number {
-//   let unique = Array.from(new Set(nums));
-//   let xor = 0;
-//   unique.forEach((el) => {
-//     xor ^= el;
+// function singleNumber(nums: number[]): number {
+//   let hash: Record<number, number> = {};
+//   let single = NaN;
+//   nums.forEach((num) => {
+//     hash[num] = (hash[num] || 0) + 1;
 //   });
-//   nums.forEach((el) => {
-//     xor ^= el;
+//   Object.entries(hash).forEach(([key, value]) => {
+//     if (value === 1) single = Number(key);
 //   });
-//   return xor;
+//   return single;
 // }
 
 /* 2 */
-function duplicateNumbersXOR(nums: number[]): number {
-  let xor = 0;
-  let hash: Record<number, number> = {};
-  nums.forEach((num) => {
-    hash[num] = (hash[num] || 0) + 1;
-    if (hash[num] == 2) xor ^= num;
-  });
-  return xor;
+// function singleNumber(nums: number[]): number {
+//   const bitCount = new Array(32).fill(0);
+//   for (const num of nums) {
+//     for (let i = 0; i < 32; i++) {
+//       if ((num & (1 << i)) !== 0) {
+//         bitCount[i]++;
+//       }
+//     }
+//   }
+//   let result = 0;
+//   for (let i = 0; i < 32; i++) {
+//     if (bitCount[i] % 3 !== 0) {
+//       result |= 1 << i;
+//     }
+//   }
+//   return result;
+// }
+
+/* 3 */ // don't know how the fuck is this working, there's no concept just calculation in this
+// function singleNumber(nums: number[]): number {
+//   let ones = 0;
+//   let twos = 0;
+//   for (let num of nums) {
+//     ones = (num ^ ones) & ~twos;
+//     twos = (num ^ twos) & ~ones;
+//   }
+//   return ones;
+// }
+
+/* 4 */
+function singleNumber(nums: number[]): number {
+  nums.sort((a, b) => a - b);
+
+  // every element appears three times except for one
+  // that's why `i=i+3`
+  for (let i = 1; i < nums.length; i = i + 3) {
+    if (nums[i] !== nums[i - 1]) return nums[i - 1];
+  }
+
+  return nums[nums.length - 1];
 }
 
-const nums = [1, 2, 3, 2, 3, 4, 4];
+const nums = [0, 1, 1, 0, 1, 0, 2];
 
-console.log(duplicateNumbersXOR(nums));
+console.log(singleNumber(nums));
